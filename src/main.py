e72fd610d496eea9315be10c1d4f1b3c534551c3
darenc9 <devoncen@gmail.com>
2024-07-22 13:53:52 -0400

    init

---
+++ src/main.py
@@ -0,0 +1,1 @@
+from src.preprocess import preprocess_image
---
+++ src/ocr.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import pytesseract
+
+# For Windows
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+

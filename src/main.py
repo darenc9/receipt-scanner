142fa606a86711e06785abc1dad5d5037df14d0a
Rightanglewrong <bennyyang996648@hotmail.com>
2024-07-28 02:51:21 -0400

    1st draft of working binarization

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
-# from src.preprocess import preprocess_image
+import cv2
+import numpy as np
+from PIL import Image
+from src.preprocess import preprocess_image
+from src.ocr import get_text
+
+image_path = "../Data/images/1005-receipt.jpg"
+image_path2 = "../Data/test-european.jpg"
 
 # read input image
+image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+image2 = cv2.imread(image_path2, cv2.IMREAD_GRAYSCALE)
 
 # obtain processed image
+processed_image = preprocess_image(image)
+p2 = preprocess_image(image2)
 
 # For Testing
 
-    # Scan unprocessed image
+# Scan unprocessed image
 
-    # Scan Read processed image
+# Scan Read processed image
+extracted_data = get_text(processed_image)
+print("Processed Image Text:")
+print(extracted_data)
+
+extracted_data2 = get_text(p2)
+print("Processed Image Text 2:")
+print(extracted_data2)
 
 # Show converted data in text
 
 # Convert data into preferred extension
 
 # Saved extracted data w/ preferred name
-
-
-
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,15 @@
 import cv2
 import numpy as np
 
-# def preprocess_image(img):
 
+def binaryization(img):
+    result_img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    return result_img
+
+def preprocess_image(img):
+    # Check if the image is already in grayscale
+    if len(img.shape) == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Generate Image Histogram
 
     # Generate MSE - Original image
@@ -17,5 +24,12 @@ import numpy as np
     # Get MSE - Each mask of equalized image
 
     # Binarization of best equalized image
+    binaried_image = binaryization(img)
+
+    # Display image
+    cv2.imshow("Binarized Image", binaried_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     # Return Image
+    return binaried_image

88b9e88791b33bbd90521b44a60601556588264d
darenc9 <devoncen@gmail.com>
2024-07-29 13:51:09 -0400

    brightness and analyze historgram

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,43 @@
 import cv2
 import numpy as np
 
+def analyze_histogram(image):
+    """
+    Analyzes the image histogram to decide if brightness or contrast adjustments are needed
+    Args: image (numpy.ndarray): Input grayscale image.
+    If needed: returns True, else returns False.
+    """
+    #Calc histogram
+    hist = cv2.calcHist([image], [0], None, [256], [0,256])
+    hist = hist / hist.sum()
+    cdf = hist.cumsum()
+
+    low_threshold = 0.01
+    high_threshold = 0.99
+
+    low_percentile = np.argmax(cdf > low_threshold)
+    high_percentile = np.argmax(cdf > high_threshold)
+
+    if low_percentile > 10:
+        return True
+    return False
+
+def adjust_brightness_contrast(image, alpha=1.5, beta=-40):
+    if analyze_histogram(image):
+        return cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return image
+
+def preprocess_image(img):
+    image = adjust_brightness_contrast(img)
+    cv2.imshow("original", img)
+    cv2.waitKey(0)
+    cv2.imshow("adjusted", image)
+    cv2.waitKey(0)
+
+image_path = f'../data/images/1002-receipt.jpg'
+image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+preprocess_image(image)
+
 # def preprocess_image(img):
 
     # Generate Image Histogram
